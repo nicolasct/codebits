cb687f2bb639482f7201d0ff4537dc9e80d2729e
nicolas <nicolas.cantono@gmail.com>
2013-04-15 17:23:09 +0100

    first version of a working VBA script for saving all .doc documents in .html, starting from a root directory

---
+++ from_doc_to_html.vb
@@ -0,0 +1,111 @@
+Sub ChangeDocsToTxtOrRTFOrHTML()
+'with export to PDF in Word 2007
+    Dim fs As Object
+    Dim oFolder As Object
+    Dim tFolder As Object
+    'Dim oFile As Object
+    Dim strDocName As String
+    Dim intPos As Integer
+    Dim locFolder As String
+    Dim fileType As String
+    On Error Resume Next
+    locFolder = InputBox("Enter the folder path to DOCs", "File Conversion", "C:\myDocs")
+    Select Case Application.Version
+        Case Is < 12
+            Do
+                fileType = UCase(InputBox("Change DOC to TXT, RTF, HTML", "File Conversion", "TXT"))
+            Loop Until (fileType = "TXT" Or fileType = "RTF" Or fileType = "HTML")
+        Case Is >= 12
+            Do
+                fileType = UCase(InputBox("Change DOC to TXT, RTF, HTML or PDF(2007+ only)", "File Conversion", "HTML"))
+            Loop Until (fileType = "TXT" Or fileType = "RTF" Or fileType = "HTML" Or fileType = "PDF")
+    End Select
+    Application.ScreenUpdating = False
+    Set fs = CreateObject("Scripting.FileSystemObject")
+    Set oFolder = fs.GetFolder(locFolder)
+    'Set tFolder = fs.CreateFolder(locFolder & "Converted")
+    'Set tFolder = fs.GetFolder(locFolder & "Converted")
+    
+    Dim files As New Collection
+    GetFilesRecursive fs.GetFolder(locFolder), "doc", files, fs
+    
+    Dim oFile As Scripting.file
+    For Each oFile In files
+        Dim d As Document
+        Set d = Application.Documents.Open(oFile.Path)
+        strDocName = ActiveDocument.Name
+        intPos = InStrRev(strDocName, ".")
+        strDocName = Left(strDocName, intPos - 1)
+        ChangeFileOpenDirectory oFile.ParentFolder
+        Select Case fileType
+        Case Is = "TXT"
+            strDocName = strDocName & ".txt"
+            ActiveDocument.SaveAs FileName:=strDocName, FileFormat:=wdFormatText
+        Case Is = "RTF"
+            strDocName = strDocName & ".rtf"
+            ActiveDocument.SaveAs FileName:=strDocName, FileFormat:=wdFormatRTF
+            
+        Case Is = "HTML"
+            strDocName = strDocName & ".html"
+            ActiveDocument.SaveAs FileName:=strDocName, FileFormat:=wdFormatFilteredHTML
+            
+                'Loop through all hyperlinks and change .doc extension for .html
+                    Dim link_to_doc As String
+                    Dim link_to_html As String
+                    Set RegEx = CreateObject("vbscript.regexp")
+                For i = 1 To ActiveDocument.Hyperlinks.Count
+                    link_to_doc = ActiveDocument.Hyperlinks(i).Address
+                        With RegEx
+                            .IgnoreCase = True
+                            .Global = True
+                            .Pattern = "(.*).doc"
+                        End With
+                        link_to_html = RegEx.Replace(link_to_doc, "$1.html")
+                    ActiveDocument.Hyperlinks(i).Address = link_to_html
+                Next
+                
+                ' Change also any display names, if they contain a ".doc" extension:
+                With ActiveDocument.Content.Find
+                    .Text = ".doc"
+                    .Replacement.ClearFormatting
+                    .Replacement.Text = ".html"
+                    .Execute Replace:=wdReplaceAll, Forward:=True, _
+                    Wrap:=wdFindContinue
+                End With
+                
+                ActiveDocument.Save
+                
+
+
+
+        Case Is = "PDF"
+            strDocName = strDocName & ".pdf"
+            ' *** Word 2007 users - remove the apostrophe at the start of the next line ***
+            'ActiveDocument.ExportAsFixedFormat OutputFileName:=strDocName, ExportFormat:=wdExportFormatPDF
+            
+        End Select
+        d.Close
+        'ChangeFileOpenDirectory oFolder
+    Next oFile
+    Application.ScreenUpdating = True
+End Sub
+
+
+
+
+
+
+Sub GetFilesRecursive(f As Scripting.Folder, filter As String, c As Collection, fso As Scripting.FileSystemObject)
+  Dim sf As Scripting.Folder
+  Dim file As Scripting.file
+
+  For Each file In f.files
+    If InStr(1, fso.GetExtensionName(file.Name), filter, vbTextCompare) = 1 Then
+      c.Add file, file.Path
+    End If
+  Next file
+
+  For Each sf In f.SubFolders
+    GetFilesRecursive sf, filter, c, fso
+  Next sf
+End Sub
